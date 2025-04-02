79e26e8508b1e195db1bf6a5cd38f1bcd023a1c5
harshalisawant12 <49194048+harshalisawant12@users.noreply.github.com>
2025-04-03 02:12:11 +0530

    commited for table.jsx

--- my-learning/src/Components/Table.jsx
+++ my-learning/src/Components/Table.jsx
@@ -6,7 +6,7 @@ import { useState } from "react";
 //Commit3 testing 
 
 
-// I am testing PR request
+// I am testing PR request in this project
 const Table = ({ rows, deleteRow, editRow }) => {
   const [edit, setEdit] = useState([]);
   const [formState, setFormState] = useState({
