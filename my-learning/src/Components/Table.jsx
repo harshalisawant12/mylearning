59a9c89108798e70df30f879ead46106e627842b
harshalisawant12 <49194048+harshalisawant12@users.noreply.github.com>
2025-04-03 02:30:36 +0530

    Feature navbar table changes (#2)
    * Initial commit for navbar branch
    
    * commited for table.jsx

--- my-learning/src/Components/Table.jsx
+++ my-learning/src/Components/Table.jsx
@@ -5,8 +5,6 @@ import "./Table.css";
 import { useState } from "react";
 //Commit3 testing 
 
-
-// I am testing PR request
 const Table = ({ rows, deleteRow, editRow }) => {
   const [edit, setEdit] = useState([]);
   const [formState, setFormState] = useState({
